2a13117e71483240941deb07285851c1f35fafa5
95markiewicz@gmail.com <95markiewicz@gmail.com>
2020-12-08 09:56:59 +0100

    Maja: klar med GainSkill

--- src/components/CollectorsGainSkill.vue
+++ src/components/CollectorsGainSkill.vue
@@ -10,7 +10,7 @@
             :card="card"
             :availableAction="card.available"
             @doAction="gainSkill(card)"/>
-          {{ cardCost(card) }}
+        <!--  {{ cardCost(card) }} -->
         </div>
       </div>
 
@@ -43,41 +43,38 @@ export default {
     labels: Object,  //specify what kind of object
     player: Object,
     skillsOnSale: Array,
-    marketValues: Object,
+    marketValues: Object, // DENNA SKA BORT
     placement: Array
     //NÅNTING MED WORK?
   },
   methods: {
     cannotAfford: function (cost) {
       let minCost = 100;
-      for(let key in this.marketValues) {
-        if (cost + this.marketValues[key] < minCost)
-          minCost = cost + this.marketValues[key]
-      }
+      //for(let key in this.marketValues) {
+        if (cost < minCost)
+          minCost = cost;
+      //}
       return (this.player.money < minCost);
     },
-    cardCost: function (card) {
-      return this.marketValues[card.market];
-    },
+  //  cardCost: function (card) {
+    //  return this.marketValues[card.market];
+    //},
+
     placeBottle: function (p) {
       this.$emit('placeBottle', p.cost);
       this.highlightAvailableCards(p.cost);
     },
-    checkAvailable: function (card, cost) {
-      if (this.marketValues[card.skill] <= this.player.money - cost) {
+    setAvailable: function (card) {
         this.$set(card, "available", true);
-      }
-      else {
-        this.$set(card, "available", false);
-      }
     },
-    highlightAvailableCards: function (cost=100) {
+    highlightAvailableCards: function () {
       for (let i = 0; i < this.skillsOnSale.length; i += 1) {
-        this.checkAvailable(this.skillsOnSale[i], cost);
+        this.setAvailable(this.skillsOnSale[i]);
       }
-      for (let i = 0; i < this.player.hand.length; i += 1) {                              //ÄVEN KORTEN PÅ HAND HIGHLIGHTAS, SÅ VILL VI EJ HA DET
-        this.checkAvailable(this.player.hand[i], cost);
-      }
+      //ÄVEN KORTEN PÅ HAND HIGHLIGHTAS, SÅ VILL VI EJ HA DET
+    //  for (let i = 0; i < this.player.hand.length; i += 1) {
+      //  this.setAvailable(this.player.hand[i]);
+    //  }
     },
     gainSkill: function (card) {
       if (card.available) {

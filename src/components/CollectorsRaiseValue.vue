4da975b62fb516aecb029bb2fb2b7b147bdec266
Karolina Hagerman <hagermankarolina@gmail.com>
2021-01-06 20:34:47 +0100

    lade till closeCross i alla popups (för stor i how to play)

--- src/components/CollectorsRaiseValue.vue
+++ src/components/CollectorsRaiseValue.vue
@@ -12,7 +12,9 @@
       <div class="popup" style= "position:relative; left:0em; top:0em;">
         <img src='/images/infoknapp_rv.png' alt="Raise value" width="70%" @click="$emit('getInfo')" >
         <span class="popuptext" id="myMarketPopup"  style= "left:10em; top:-3em;">
-          <h1>{{ labels.raiseValue }}</h1>
+          <input class="closeCross" type="image" @click="$emit('getInfo')" alt="Login"
+          src='/images/close.png' >
+          <h2>{{ labels.raiseValue }}</h2>
           {{ labels.valueInfo }}
           <br>
           <button class="closeButton"  @click="$emit('getInfo')">close</button>
